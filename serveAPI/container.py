29bf76edfbef8d016b07bf16bb1d2180b047d6b0
RBELLI <bellirodrigo2@gmail.com>
2025-04-20 11:04:43 -0300

    ...

--- serveAPI/container.py
+++ serveAPI/container.py
@@ -1,11 +1,19 @@
-from serveAPI.di import DependencyInjector, IoCContainer
+from typing import Any
+
+from serveAPI import middleware
+from serveAPI.di import DependencyInjector, Depends, IoCContainer
+from serveAPI.dispatcher import Dispatcher
 from serveAPI.exceptionhandler import ExceptionRegistry
+from serveAPI.input_types.str_input import IntrusiveStrEncoder, MiddlewareStr
 from serveAPI.router import RouterAPI
 from serveAPI.safedict import SafeDict
+from serveAPI.spawns.asyncio_spawn import AsyncioSpawn
+
+SafeDictTaskID = SafeDict[str | tuple[str, int]]
 
 
-def provide_safe_dict_dispatcher(_: IoCContainer) -> SafeDict[str | tuple[str, int]]:
-    return SafeDict[str | tuple[str, int]]()
+def provide_safe_dict_dispatcher(_: IoCContainer) -> SafeDictTaskID:
+    return SafeDictTaskID()
 
 
 def provide_exception(_: IoCContainer) -> ExceptionRegistry:
@@ -18,3 +26,24 @@ def provide_router(_: IoCContainer) -> RouterAPI:
 
 def provide_di(_: IoCContainer) -> DependencyInjector:
     return DependencyInjector()
+
+
+def make_dispatcher(
+    encoder=Depends(IntrusiveStrEncoder),
+    middleware=Depends(MiddlewareStr),
+    spawn=Depends(AsyncioSpawn),  # aqui acho que tem que ser lambda
+    exception=Depends(ExceptionRegistry),
+    safedict=Depends(SafeDictTaskID),
+):
+    return Dispatcher[Any](
+        encoder=encoder,
+        middleware=middleware,
+        spawn=spawn,
+        exception_handlers=exception,
+        server=None,
+        registry=safedict,
+    )
+
+
+# use case
+# di.resolve(make_dispatcher)
--- serveAPI/spawns/asyncio_spawn.py
+++ serveAPI/spawns/asyncio_spawn.py
@@ -1,12 +1,14 @@
 import asyncio
 from typing import Any, Callable, Coroutine
 
-from serveAPI.ioc import IoCContainer
+from serveAPI.di import IoCContainer
 
 asyncio_spawn = asyncio.create_task
+
+AsyncioSpawn = Callable[[Coroutine[Any, Any, Any]], asyncio.Task[Any]]
 
 
 def provide_spawn(
     _: IoCContainer,
-) -> Callable[[Coroutine[Any, Any, Any]], asyncio.Task[Any]]:
+) -> AsyncioSpawn:
     return asyncio_spawn
